dc046d67e4941dcaf1f92fdcd6729b19c52049bf
Divjot Singh <bogas04@gmail.com>
2015-08-21 19:21:43 +0530

    cats

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+(function (d,s,tw_id,fb_id){ var js, fjs = d.getElementsByTagName(s)[0]; if(!d.getElementById(tw_id)) { js = d.createElement(s); js.id = tw_id; js.src = "//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js,fjs); } if (!d.getElementById(fb_id)) { js = d.createElement(s); js.id = fb_id; js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=603267456389915"; fjs.parentNode.insertBefore(js, fjs); } } (document, "script", "twitter-wjs", 'facebook-jssdk'));
+var sc_project=10571971; 
+var sc_invisible=1; 
+var sc_security="952925b7"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+    "https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+    "statcounter.com/counter/counter.js'></"+"script>");
