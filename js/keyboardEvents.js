bf8ab06b77341ebf8ac1e036b3b2656b8b871005
Divjot Singh <bogas04@gmail.com>
2015-09-15 20:23:05 +0530

    using charCode instead of key

--- js/keyboardEvents.js
+++ js/keyboardEvents.js
@@ -1,10 +1,11 @@
 window.onkeypress = function(e) {
+  console.log(e);
   this.currentSection = this.currentSection || 0;
   this.$sections = this.$sections || document.getElementsByClassName('section');
-  if((e.key === "j" || e.keyCode === 40) && this.currentSection < this.$sections.length - 1) {
+  if((e.charCode === 106 || e.keyCode === 40) && this.currentSection < this.$sections.length - 1) {
     window.scrollTo(0, this.$sections[++this.currentSection].offsetTop);
     return false;
-  } else if((e.key === "k" || e.keyCode === 38) && this.currentSection > 0) {
+  } else if((e.charCode === 107 || e.keyCode === 38) && this.currentSection > 0) {
     window.scrollTo(0, this.$sections[--this.currentSection].offsetTop);
     return false;
   }
