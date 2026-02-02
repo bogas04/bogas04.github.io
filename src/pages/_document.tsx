53fd51125356216eaaed200f0aa01f66f097a70b
bogas04 <bogas04@gmail.com>
2026-02-02 16:13:09 +0530

    chore: add telemetry

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* GoatCounter analytics */}
+          <script
+            data-goatcounter="https://bogas04.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
