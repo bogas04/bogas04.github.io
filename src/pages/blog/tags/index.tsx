9357c1de44152d449bbd640a1c688ba5a8b52d23
bogas04 <bogas04@gmail.com>
2026-07-27 16:48:23 +0530

    fix: more stuff is fixed now

--- src/pages/blog/tags/index.tsx
+++ src/pages/blog/tags/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import type { CSSProperties } from "react";
 import BlogLayout from "../../../layout/blog";
 import { getBlogPostSummaries } from "../../../utils/blog";
 import { getBlogTagColors, getBlogTagSlug } from "../../../utils/blogTag";
@@ -25,33 +26,30 @@ interface TagsIndexProps {
 }
 
 function TagsIndex({ tags }: TagsIndexProps) {
-  const maxCount = Math.max(...tags.map((tag) => tag.count), 1);
-
   return (
     <BlogLayout
-      title="blog tags"
+      title="tags"
       breadcrumbs={[
         { href: "/", label: "divjot" },
         { href: "/blog", label: "blog" },
         { label: "tags" },
       ]}
     >
-      <ul className="m-0 flex flex-row flex-wrap gap-4 p-0 [@supports(grid-template-rows:masonry)]:grid [@supports(grid-template-rows:masonry)]:grid-cols-[repeat(auto-fill,minmax(8rem,1fr))] [@supports(grid-template-rows:masonry)]:[grid-template-rows:masonry] [@supports(grid-template-rows:masonry)]:[align-tracks:start] [&>li]:[@supports(grid-template-rows:masonry)]:break-inside-avoid">
+      <ul className="m-0 flex flex-wrap gap-3 p-0">
           {tags.map((tag) => (
             <li key={tag.name}>
               <Link
                 href={`/blog/tags/${getBlogTagSlug(tag.name)}`}
-                className="inline-flex items-center border border-[var(--tag-text-color)] bg-[var(--tag-background-color)] p-0 no-underline text-[var(--tag-text-color)] dark:!border-[var(--tag-text-color-dark)] dark:!bg-[var(--tag-background-color-dark)] dark:!text-[var(--tag-text-color-dark)]"
+                className="blog-tag transition-opacity hover:opacity-80"
                 style={{
                   "--tag-background-color": getBlogTagColors(tag.name).backgroundColor,
                   "--tag-text-color": getBlogTagColors(tag.name).textColor,
                   "--tag-background-color-dark": getBlogTagColors(tag.name).darkBackgroundColor,
                   "--tag-text-color-dark": getBlogTagColors(tag.name).darkTextColor,
-                  fontSize: `${0.9 + (tag.count / maxCount) * 0.9}em`,
-                } as React.CSSProperties}
+                } as CSSProperties}
               >
-                <span className="px-[0.45em] py-[0.2em]">{tag.name}</span>
-                <span className="inline-flex self-stretch items-center border-l border-current px-[0.45em]">
+                <span className="tag-name">{tag.name}</span>
+                <span className="tag-count" aria-label={`${tag.count} posts`}>
                   {tag.count}
                 </span>
               </Link>
